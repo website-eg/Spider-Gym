f6143861f759bc5f90b9d1a04de75686bd977409
ninjacode <ninjacode2006@gmail.com>
2023-07-11 18:08:05 +0300

    fixed

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+let button = document.querySelector("#btn");
+let menu = document.querySelector("#menu");
+let listMenu = document.querySelector("#list-menu");
+let listMenu_links = document.querySelectorAll(".link");
+
+// Scroll down when button is clicked
+button.addEventListener("click", function () {
+  window.scrollBy({ top: 700, behavior: "smooth" });
+});
+
+// Show menu when menu icon is clicked
+menu.addEventListener("click", function (e) {
+  e.stopPropagation();
+  listMenu.classList.add("show");
+  menu.classList.add("hidden");
+});
+
+// Prevent closing when clicking inside the menu
+listMenu.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+// Close menu when clicking outside
+document.body.addEventListener("click", function () {
+  listMenu.classList.remove("show");
+  menu.classList.remove("hidden");
+});
+
+// Hide menu when any link is clicked
+listMenu_links.forEach(function (link) {
+  link.addEventListener("click", function () {
+    listMenu.classList.remove("show");
+    menu.classList.remove("hidden");
+  });
+});
