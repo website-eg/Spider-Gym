cb647725209797f4a03e80dd60f437138360adb5
ninjacode <ninjacode2006@gmail.com>
2025-05-16 00:28:34 +0300

    Add some updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,26 @@
 let button = document.querySelector("#btn");
 let menu = document.querySelector("#menu");
-let closeMenu = document.querySelector("#close-menu");
+let listMenu = document.querySelector("#list-menu");
 
+// Scroll down when button is clicked
 button.addEventListener("click", function () {
   window.scrollBy({ top: 700, behavior: "smooth" });
 });
 
-closeMenu.addEventListener("click", function () {
-  document.querySelector("#list-menu").classList.add("hidden");
-  closeMenu.classList.add("hidden");
-  menu.classList.remove("hidden");
+// Show menu when menu icon is clicked
+menu.addEventListener("click", function (e) {
+  e.stopPropagation(); // Prevent body click from firing
+  listMenu.classList.remove("hidden");
+  menu.classList.add("hidden");
 });
 
-menu.addEventListener("click", function () {
-  document.querySelector("#list-menu").classList.remove("hidden");
-  closeMenu.classList.remove("hidden");
-  menu.classList.add("hidden");
+// Prevent closing when clicking inside the menu
+listMenu.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+// Close menu when clicking outside
+document.body.addEventListener("click", function () {
+  listMenu.classList.add("hidden");
+  menu.classList.remove("hidden");
 });
